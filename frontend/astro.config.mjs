30c1e05a1b9873218eee8de66c1dc5c5254440f0
Estremadura, Ric Michael <ricmichaele@gmail.com>
2026-07-16 13:54:30 +0800

    production finished

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -3,6 +3,15 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  env: {
+    schema: {
+      YOUTUBE_API_KEY: { 
+        context: 'server', 
+        access: 'secret', 
+        type: 'string' 
+      },
+    },
+  },
   vite: {
     plugins: [tailwindcss()],
   },
