357fa052ef97359e2882049d218dd60ec62afc3c
Estremadura, Ric Michael <ricmichaele@gmail.com>
2026-07-23 12:45:35 +0800

    Cloudflare adapter installment

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -2,6 +2,8 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
+import cloudflare from '@astrojs/cloudflare';
+
 export default defineConfig({
   env: {
     schema: {
@@ -12,7 +14,10 @@ export default defineConfig({
       },
     },
   },
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  adapter: cloudflare(),
 });
